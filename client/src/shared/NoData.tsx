a4f0ea60c02035fa74080b2606a6fb4e8e33346b
Onni Rantanen <ounaifi@gmail.com>
2023-10-30 21:31:09 +0200

    Shared no data component

---
+++ client/src/shared/NoData.tsx
@@ -0,0 +1,27 @@
+interface Props {
+  name: string
+  loading: boolean
+  error: Error | null
+}
+
+type NameProp = Pick<Props, 'name'>;
+type ErrorProps = NameProp & { error: Error };
+
+const LoadingMessage = ({ name }: NameProp) => (
+  <div>
+    Loading {name} data...
+  </div>
+);
+
+const ErrorMessage = ({ name, error }: ErrorProps) => (
+  <div>
+    Failed to fetch {name} data: {error.message}
+  </div>
+);
+
+export const NoData = ({ name, loading, error }: Props) =>
+  loading
+    ? <LoadingMessage name={name} />
+    : error
+      ? <ErrorMessage name={name} error={error} />
+      : <ErrorMessage name={name} error={new Error('No data')} />;
