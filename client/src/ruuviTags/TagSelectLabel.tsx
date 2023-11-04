44f881feb6420435060637743f77e7154fd55f47
Onni Rantanen <ounaifi@gmail.com>
2023-11-04 09:40:01 +0200

    Better formatting in RuuviTag selector

--- client/src/ruuviTags/TagSelectLabel.tsx
+++ client/src/ruuviTags/TagSelectLabel.tsx
@@ -1,4 +1,4 @@
-import type { ChangeEvent } from 'react';
+import type { ChangeEvent, CSSProperties } from 'react';
 
 import Form from 'react-bootstrap/Form';
 
@@ -7,6 +7,36 @@ import { useDispatch, useSelector } from '../state';
 import { setRuuviTagName, selectRuuviTagSelections } from '../state/ruuviTagSelections';
 
 import type { RuuviTagData } from '.';
+
+const getLastSeenString = (date: Date): string => {
+  const result = [];
+
+  let s = Math.floor((new Date().getTime() - date.getTime()) / 1000);
+
+  if (s >= 60) {
+    let m = Math.floor(s / 60);
+
+    s %= 60;
+
+    if (m >= 60) {
+      const h = Math.floor(m / 60);
+
+      if (h >= 24) {
+        return 'a long time';
+      }
+
+      m %= 60;
+
+      result.push(`${h}h`);
+    }
+
+    result.push(`${m}m`);
+  }
+
+  result.push(`${s}s`);
+
+  return result.join(' ');
+};
 
 interface Props {
   tag: RuuviTagData
@@ -18,7 +48,7 @@ export const TagSelectLabel = ({ tag, checked }: Props) => {
   const ruuviTagSelections = useSelector(selectRuuviTagSelections);
 
   const selection = ruuviTagSelections.find(({ tagId }) => tag.tagId === tagId);
-  const lastSeenS = Math.round((new Date().getTime() - new Date(tag.updatedAt).getTime()) / 1000);
+  const lastSeen = getLastSeenString(new Date(tag.updatedAt));
 
   const onChangeRoom = (event: ChangeEvent<HTMLSelectElement>) => {
     dispatch(setRuuviTagName({
@@ -27,22 +57,32 @@ export const TagSelectLabel = ({ tag, checked }: Props) => {
     }));
   };
 
-  const separator = (
-    <span style={{ marginLeft: '20px' }}></span>
-  );
+  const itemStyle: CSSProperties = {
+    marginRight: '20px',
+    minWidth: '70px',
+    display: 'inline-block'
+  };
 
   return (
     <>
       RuuviTag <code>{tag.tagId}</code>
 
-      <div>
-        {tag.rssi} dBm
-        {separator}
-        {Number(tag.temperature).toFixed(1)} °C
-        {separator}
-        {Number(tag.humidity).toFixed(0)} %
-        {separator}
-        {lastSeenS}s ago
+      <div style={{ whiteSpace: 'nowrap' }}>
+        <div style={itemStyle}>
+          {tag.rssi} dBm
+        </div>
+
+        <div style={itemStyle}>
+          {Number(tag.temperature).toFixed(1)} °C
+        </div>
+
+        <div style={itemStyle}>
+          {Number(tag.humidity).toFixed(0)} %
+        </div>
+
+        <div style={itemStyle}>
+          {lastSeen} ago
+        </div>
       </div>
 
       {checked && (
